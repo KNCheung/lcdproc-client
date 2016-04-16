c18a9848356336e6fadcf02f0b07c9ac87d688a1
KNCheung <beobachter70@163.com>
2016-04-16 18:09:52 +0800

    add AM2302's data to LargeClock

--- screens/LargeClock.py
+++ screens/LargeClock.py
@@ -6,20 +6,37 @@ from datetime import datetime
 now = datetime.now
 import psutil
 
+import Adafruit_DHT as dht
+
 class LargeClock(ScreenBase):
     def run(self): 
+        pin = self.intConfig('sensorPort', 18)
+        
         h1 = self.screen.add_number_widget("h1", x=1, value=0)
         h2 = self.screen.add_number_widget("h2", x=4, value=0)
         m1 = self.screen.add_number_widget("m1", x=8, value=0)
         m2 = self.screen.add_number_widget("m2", x=11, value=0)
-        sec = self.screen.add_string_widget("sec", '--', x=15, y=2)
+        temp = self.screen.add_string_widget("temp", '--', x=15, y=1)
+        humidity = self.screen.add_string_widget("humidity", '--', x=15, y=2)
         dot1 = self.screen.add_string_widget("d1", ".", x=7, y=1)
         dot2 = self.screen.add_string_widget("d2", ".", x=7, y=2)
         while True:
-            t = now()
-            h1.set_value(t.hour / 10)
-            h2.set_value(t.hour % 10)
-            m1.set_value(t.minute / 10)
-            m2.set_value(t.minute % 10)
-            sec.set_text('{0:02d}'.format(t.second))
-            sleep(1)
+            try:
+                h, t = dht.read_retry(dht.AM2302, pin, retries=5, delay_seconds=2)
+            except RuntimeError:
+                h, t = (None, None)
+
+            if h or t:
+                temp.set_text("{0:02.0f}".format(t))
+                humidity.set_text("{0:02.0f}".format(h))
+            else:
+                temp.set_text("--")
+                humidity.set_text("--")
+
+            for _ in range(12):
+                t = now()
+                h1.set_value(t.hour / 10)
+                h2.set_value(t.hour % 10)
+                m1.set_value(t.minute / 10)
+                m2.set_value(t.minute % 10)
+                sleep(5)
